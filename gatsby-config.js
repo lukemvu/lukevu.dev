132b3a8bb42934be56461874b0f8beb965e833c7
luke <luke@eos6-desktop.localdomain>
2021-10-16 16:35:50 -0400

    test commits

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    title: 'Luke Vu',
+    description: 'Luke Vu is a software engineer who specializes in literally nothing.',
+    siteUrl: 'https://lukevu.dev', // No trailing slash allowed!
+    image: '/noted.gif', // Path to your image you placed in the 'static' folder
+    twitterUsername: ' ',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'LukeVu',
+        short_name: 'LukeVu',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,10 +136,8 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Luke and I enjoy creating things that live nowhere. My interest in
+              nothing started back in 2007 when I decided to edit my Neopets store shop.
             </p>
 
             <p>
