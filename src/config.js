7a4ab1c863ea13ce226a03873166dec6c72d8d80
Luke <luke@ThinkPad-X1-Carbon-Gen-8-2ba59a43.localdomain>
2021-10-17 10:48:50 -0400

    updates

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'luke.m.vu@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/lukemvu',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/spaztec',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/lukemvu',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/lukevu',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    // {
+    // name: 'Codepen',
+    // url: 'https://codepen.io/bchiang7',
+    // },
   ],
 
   navLinks: [
